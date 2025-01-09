8d9204cde06b683c0013a4cd96a70f8324cb2051
unkitgolem1 <minedanigerman@gmail.com>
2025-01-08 22:58:35 -0600

    Update ohllama.py

--- ohllama.py
+++ ohllama.py
@@ -3,3 +3,5 @@ class ia:
 	def ia (self, pregunta = ''):		
 		response= generate('llama2-uncensored',pregunta)
 		return response['response']
+
+#Para usar esa madre tenemos que tener ollama installado en local
