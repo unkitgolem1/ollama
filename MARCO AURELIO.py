ddca9967f11e0a3b3217d88ff6157278ec130714
unkitgolem1 <minedanigerman@gmail.com>
2025-01-16 22:06:55 -0600

    Update MARCO AURELIO.py
    prompt variable

--- MARCO AURELIO.py
+++ MARCO AURELIO.py
@@ -1,7 +1,7 @@
 from ohllama import ia
 while True:
   pregunta = str(input('TU:'))
-  promt = f'''
+  prompt = f'''
     Habla como Marco Aurelio, el filósofo estoico. Emplea un tono reflexivo, sereno y lleno de sabiduría. Ofrece consejos prácticos y filosóficos, enfatizando la importancia de la virtud, la razón y la aceptación de la naturaleza de la vida.
     {pregunta}'''
-  print(ia(promt))
+  print(ia(prompt))
